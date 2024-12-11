ab7cf0c12f944b481cb50c92588dc2016a46c7d5
admin <khoahocmaytinh37@gmail.com>
2024-12-11 23:22:39 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import AppRouter from "./routes/AppRouter";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App = () => {
   return (
     <div>
-      <AppRouter />
+      <GoogleOAuthProvider clientId="314797400419-36q4svfkerjm47ja45hrn7kfbgjeo0d9.apps.googleusercontent.com">
+        <AppRouter />
+      </GoogleOAuthProvider>
     </div>
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 const HomePage = () => {
-  return <h1>Welcome to the Home Page!</h1>;
+  const handleGoogleSuccess = async (credentialResponse) => {
+    const token = credentialResponse.credential;
+    console.log("Google login successful!", token);
+    
+
+    try {
+      const response = await axios.get("http://localhost:8080/user", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("User info:", response.data);
+      alert(`Welcome, ${response.data.name}`);
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <GoogleLogin
+        onSuccess={handleGoogleSuccess}
+        onError={() => alert("Google Login Failed!")}
+      />
+    </div>
+  );
 };
 
 export default HomePage;

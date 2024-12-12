41e365d4d16f489665447221495c5baa0f14bd8f
vietnq <>
2024-12-12 17:10:01 +0700

    commit

--- src/component/account/login/Login.js
+++ src/component/account/login/Login.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../../account/account.css';
+import LanguageSwitcher from '../../multilingual/LanguageSwitcher';
+import { useTranslation } from 'react-i18next';
 
 const Login = () => {
+    const { t } = useTranslation();
 
     const handelSubmit = (e) => {
         e.preventDefault();
@@ -13,9 +16,10 @@ const Login = () => {
             <div className='header-login'>
                 <div className='d-flex w-50'>
                     <div className='w-50'>tes</div>
-                    <div className='d-flex w-50 justify-content-end text-danger'>Bạn cần giúp đỡ?</div>
+                    <div className='d-flex w-50 justify-content-end text-danger'>{t("need_help")}</div>
                 </div>
             </div>
+             <LanguageSwitcher />
         </form>
     );
 };
---
+++ src/component/multilingual/LanguageSwitcher.js
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useLoading } from "../loading/LoadingProvider";
+
+const languages = [
+    { code: "en", lang: "English" },
+    { code: "vi", lang: "Việt Nam" },
+];
+
+const LanguageSelector = () => {
+    const { showLoading, hideLoading } = useLoading();
+    const { i18n } = useTranslation();
+
+    useEffect(() => {
+        document.body.dir = i18n.dir();
+    }, [i18n, i18n.language]);
+
+    const changeLanguage = (lng) => {
+        showLoading();
+        i18n.changeLanguage(lng)
+            .finally(() => hideLoading());
+    };
+
+    return (
+        <div className="btn-container">
+            {languages.map((lng) => {
+                return (
+                    <button
+                        className={lng.code === i18n.language ? "selected" : ""}
+                        key={lng.code}
+                        onClick={() => changeLanguage(lng.code)}
+                    >
+                        {lng.lang}
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
+
+export default LanguageSelector;

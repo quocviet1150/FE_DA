ca2da5f9e2c1716a32fd9f093496025af9ad82d4
admin <khoahocmaytinh37@gmail.com>
2024-12-17 23:30:08 +0700

    commit code

--- src/admin/view/dashboard/dashboard.js
+++ src/admin/view/dashboard/dashboard.js
@@ -1,15 +1,80 @@
+import { CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react';
+import {
+    CChartBar,
+    CChartLine
+} from '@coreui/react-chartjs';
 import React from 'react';
 import './dashboard.css';
 
 const Dashboards = () => {
+    const random = () => Math.round(Math.random() * 100)
     return (
         <>
             <div className="breadcrumb">
                 <span className="breadcrumb-active">Dashboard</span>
             </div>
 
-            <div className="background-f3f4f7">
-                
+            <div className="background-f3f4f7" style={{ padding: '1%' }}>
+                <CRow>
+                    <CCol xs={12}>
+                    </CCol>
+                    <CCol xs={6}>
+                        <CCard className="mb-4" style={{ height: '60vh' }}>
+                            <CCardHeader>
+                                Bar Chart
+                            </CCardHeader>
+                            <CCardBody>
+                                <CChartBar
+                                    data={{
+                                        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                                        datasets: [
+                                            {
+                                                label: 'GitHub Commits',
+                                                backgroundColor: '#f87979',
+                                                data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
+                                            },
+                                        ],
+                                    }}
+                                    labels="months"
+                                />
+                            </CCardBody>
+                        </CCard>
+                    </CCol>
+
+                    <CCol xs={6}>
+                        <CCard className="mb-4" style={{ height: '60vh' }}>
+                            <CCardHeader>
+                                Line Chart
+                            </CCardHeader>
+                            <CCardBody>
+                                <CChartLine
+                                    data={{
+                                        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                                        datasets: [
+                                            {
+                                                label: 'My First dataset',
+                                                backgroundColor: 'rgba(220, 220, 220, 0.2)',
+                                                borderColor: 'rgba(220, 220, 220, 1)',
+                                                pointBackgroundColor: 'rgba(220, 220, 220, 1)',
+                                                pointBorderColor: '#fff',
+                                                data: [random(), random(), random(), random(), random(), random(), random()],
+                                            },
+                                            {
+                                                label: 'My Second dataset',
+                                                backgroundColor: 'rgba(151, 187, 205, 0.2)',
+                                                borderColor: 'rgba(151, 187, 205, 1)',
+                                                pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+                                                pointBorderColor: '#fff',
+                                                data: [random(), random(), random(), random(), random(), random(), random()],
+                                            },
+                                        ],
+                                    }}
+                                />
+                            </CCardBody>
+                        </CCard>
+                    </CCol>
+                </CRow>
+
             </div>
         </>
     );
